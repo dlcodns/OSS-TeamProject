33d178a5462d942eda66f18484e21d6ad309e807
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2022-10-13 18:05:09 +0900

    Create 코드분할.c

---
+++ 코드분할.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+  int x, y;
+  
+  printf("Enter two integer : ");
+  scanf("%d %d", &x, &y);
+  printf("Add : %d + %d = %d\n", x, y, Add(x, y));
+  printf("Sub : %d - %d = %d\n", x, y, Sub(x, y));
+  printf("Mul : %d * %d = %d\n", x, y, Mul(x, y));
+  if (y == 0) {
+		printf("Div : can't define.");
+	}
+	else {
+		printf("Div : %d / %d = %.1f", x, y, Div(x, y));
+	}
+  return 0;
+}
