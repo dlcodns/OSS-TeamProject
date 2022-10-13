b1607a8120b5787a731d3d366e29898803ffc781
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2022-10-14 01:52:19 +0900

    채운 - 코드 정리

--- 코드분할.c
+++ 코드분할.c
@@ -2,17 +2,17 @@
 
 int Add(int x, int y)
 {
-	return x+y;
+	return x + y;
 }
 
 int Sub(int x, int y)
 {
-  return x - y;
+	return x - y;
 }
 
 int Mul(int x, int y)
 {
-	return x*y;
+	return x * y;
 }
 
 float Div(int x, int y)
@@ -20,20 +20,20 @@ float Div(int x, int y)
 	return (float)x / y;
 }
 
-int main()
+int main(void)
 {
-  int x, y;
+	int x, y;
   
-  printf("Enter two integer : ");
-  scanf("%d %d", &x, &y);
-  printf("Add : %d + %d = %d\n", x, y, Add(x, y));
-  printf("Sub : %d - %d = %d\n", x, y, Sub(x, y));
-  printf("Mul : %d * %d = %d\n", x, y, Mul(x, y));
-  if (y == 0) {
-		printf("Div : can't define.");
+	printf("Enter two integer : ");
+	scanf("%d %d", &x, &y);
+	printf("Add : %d + %d = %d\n", x, y, Add(x, y));
+	printf("Sub : %d - %d = %d\n", x, y, Sub(x, y));
+	printf("Mul : %d * %d = %d\n", x, y, Mul(x, y));
+	if (y == 0) {
+		printf("Div : Can't define.");
 	}
 	else {
 		printf("Div : %d / %d = %.1f", x, y, Div(x, y));
 	}
-  return 0;
+	return 0;
 }
